65daf7f6555ae5b0d518b06e929754decc18c27e
Julia <ibralenko6724809@gmail.com>
2023-06-23 23:56:46 +0300

    format document

--- extra-hw/index.js
+++ extra-hw/index.js
@@ -1,27 +1,27 @@
 const numbers = [];
-  let i = -10;
+let i = -10;
 
-  while (i <= 10) {
-    numbers.push(i);
-    i++;
-  };
+while (i <= 10) {
+  numbers.push(i);
+  i++;
+};
 
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < 0) {
-      numbers.splice(i, 1);
-      i--;
-    } else {
-      numbers[i] = numbers[i] ** 2;
-    }
-  };
-
-  numbers.sort((a, b) => b - a);
-
-  const makeUniq = (arr) => {
-    const uniqSet = new Set(arr);
-    return [...uniqSet];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < 0) {
+    numbers.splice(i, 1);
+    i--;
+  } else {
+    numbers[i] = numbers[i] ** 2;
   }
-  makeUniq(numbers);
+};
 
-  console.log(numbers);
+numbers.sort((a, b) => b - a);
+
+const makeUniq = (arr) => {
+  const uniqSet = new Set(arr);
+  return [...uniqSet];
+}
+makeUniq(numbers);
+
+console.log(numbers);
