463fa362a3e02ba80145f5b8c67a33ba4f6bf334
Julia <ibralenko6724809@mail.ru>
2023-06-23 12:57:47 +0300

    fix temperatures push

--- cities/index.js
+++ cities/index.js
@@ -1,11 +1,11 @@
 
-const citiesList = document.querySelector('#cities');
+const citiesList = document.querySelector('#cities-list');
 const cities = ["Москва", "Санкт-Петербург", "Нью-Йорк", "Токио"];
 const temperatures= [];
 
 for (let i = 0; i < cities.length; i++) {
     const tempInput = prompt(`Введите температуру для ${cities[i]}`);
-    tempInput.push(Number(tempInput));
+    temperatures.push(Number(tempInput));
     const cityElement = document.createElement('li');
     cityElement.textContent = `Температура в городе ${cities[i]}: ${tempInput} °С`;
     citiesList.append(cityElement);
